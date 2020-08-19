d4e23d66d52d5428c3db42e1c4a7ac3307c53705
arturadamian <arturadamian@gmail.com>
2020-08-18 23:34:59 -0700

    Updates

---
+++ precache-manifest.c7981ac876de56ef628281f9dbac01c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c5e020fb6d31aac21c269850715a3ed",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "518dc8b135505495c018",
+    "url": "/dashboard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "43175e84fdfafc547eb2",
+    "url": "/dashboard/static/js/2.1baaf7bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dashboard/static/js/2.1baaf7bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "518dc8b135505495c018",
+    "url": "/dashboard/static/js/main.93d1ebd2.chunk.js"
+  },
+  {
+    "revision": "8f3ec870dce268db1d96",
+    "url": "/dashboard/static/js/runtime-main.2ec0fa51.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
